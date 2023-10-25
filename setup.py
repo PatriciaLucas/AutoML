5d89fc16f54c737ad5cd1f5564d48e0dbba0f018
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2023-10-25 18:15:46 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
+import setuptools
 
+setuptools.setup(
+    name='CATS',
+    install_requires=[
+          'matplotlib',
+          'numpy',
+          'pandas'],
+    packages=[],	
+    version='1.0',
+    description='AUTOML Causal para previsão de séries temporais',
+    long_description='AUTOML Causal para previsão de séries temporais',
+    long_description_content_type="text/markdown",
+    author='Patrícia de Oliveira e Lucas',
+    author_email='patelucas@gmail.com',
+    url='',
+    download_url='',
+    keywords=['forecasting', 'causal'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+    
+    ]
+)

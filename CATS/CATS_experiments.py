c7b2278ec012cdf2a595f99c08e3583ce2b19331
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2023-11-26 21:33:41 -0300

    Update CATS_experiments.py

--- CATS/CATS_experiments.py
+++ CATS/CATS_experiments.py
@@ -56,7 +56,7 @@ def execute_cats(name_dataset, dataset, target, step_ahead, database_path):
 
 
 
-def run(num_experiments=10, datasets, target):
+def run(datasets, target, num_experiments):
   step_ahead = 10
   database_path = 'bd_cats.db'
 
